c3025d81c10ac0ff19adcb886ffc5b3fb72c2884
vivek <vivekkumarpatel0312@gmail.com>
2024-03-13 09:48:15 +0530

    initial project and route setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+    const express = require('express');
+    require('dotenv').config();
+    const chats   = require('./data/data')
+    const app = express();
+
+    app.get('/', (req,res) => {
+        res.send('API is running')
+    })
+
+    app.get('/api/chat', (req, res) => {
+        res.send(chats)
+    })
+
+    app.get('/api/chat/:id', (req, res) => {
+        const singleChat  = chats.find(c => c._id === req.params.id);
+        res.send(singleChat)
+    })
+
+
+    const PORT = process.env.PORT || 5000
+
+    app.listen(5000, () => {
+        console.log(`Server is running on ${PORT}`);
+    })
