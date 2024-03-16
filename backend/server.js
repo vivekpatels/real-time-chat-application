912f965a1f0b6f44f506f6d4acd55a7be9023752
vivek <vivekkumarpatel0312@gmail.com>
2024-03-16 12:54:45 +0530

    Routes added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
     const express = require('express');
     require('dotenv').config();
     const chats   = require('./data/data')
+    const cors = require('cors')
     const app = express();
+
+    app.use(cors())
+    app.use(express.json());
 
     app.get('/', (req,res) => {
         res.send('API is running')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Homepage from './pages/Homepage'
+import Chatpage from './pages/Chatpage'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/chats" element={<Chatpage />} />
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
