e6f74ceb92f7246c0f64017c214d3d44b1f5b42c
vivek <vivekkumarpatel0312@gmail.com>
2024-03-16 17:26:20 +0530

    Authentication and Login & Signup Added

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,22 @@
     const express = require('express');
     require('dotenv').config();
     const chats   = require('./data/data')
-    const cors = require('cors')
+    const cors = require('cors');
+    const connectDB = require('./config/db');
     const app = express();
-
+    
+    const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+    
+    connectDB()
     app.use(cors())
     app.use(express.json());
 
-    app.get('/', (req,res) => {
-        res.send('API is running')
-    })
+    app.use('/api/user', userRoutes)
+    
+    app.use(notFound);
+    app.use(errorHandler)
 
-    app.get('/api/chat', (req, res) => {
-        res.send(chats)
-    })
-
-    app.get('/api/chat/:id', (req, res) => {
-        const singleChat  = chats.find(c => c._id === req.params.id);
-        res.send(singleChat)
-    })
 
 
     const PORT = process.env.PORT || 5000
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,64 @@
-import React from 'react'
-
-const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
-
-export default Homepage
+import {
+    Box,
+    Container,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text,
+  } from "@chakra-ui/react";
+  import { useEffect } from "react";
+  import { useNavigate } from "react-router-dom";
+  import Login from "../components/Authentication/Login";
+  import Signup from "../components/Authentication/Signup";
+  import "../App.css"
+  
+  function Homepage() {
+    const history = useNavigate();
+  
+    useEffect(() => {
+      const user = JSON.parse(localStorage.getItem("userInfo"));
+  
+      if (user) history.push("/chats");
+    }, [history]);
+  
+    return (
+    <div className="App">
+      <Container maxW="xl" centerContent>
+        <Box
+          d="flex"
+          justifyContent="center"
+          p={3}
+          bg="white"
+          w="100%"
+          m="40px 0 15px 0"
+          borderRadius="lg"
+          borderWidth="1px"
+        >
+          <Text fontSize="4xl" fontFamily="Work sans">
+            Talk-A-Tive
+          </Text>
+        </Box>
+        <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+          <Tabs isFitted variant="soft-rounded">
+            <TabList mb="1em">
+              <Tab>Login</Tab>
+              <Tab>Sign Up</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Login />
+              </TabPanel>
+              <TabPanel>
+                <Signup />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Container>
+      </div>
+    );
+  }
+  
+  export default Homepage;
